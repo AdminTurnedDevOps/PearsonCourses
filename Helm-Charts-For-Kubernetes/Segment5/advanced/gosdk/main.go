2469f46d396b9548e91ea45feaad3c62a77170b0
Michael Levan <mlevan1992@gmail.com>
2022-07-28 13:03:43 -0400

    go update

--- Helm-Charts-For-Kubernetes/Segment5/advanced/gosdk/main.go
+++ Helm-Charts-For-Kubernetes/Segment5/advanced/gosdk/main.go
@@ -13,26 +13,24 @@ func main() {
 	settings := cli.New()
 
 	// Create a new instance of the configuration
-	actionConfig := new(action.Configuration)
+	config := new(action.Configuration)
 
 	// Collect local Helm information
-	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+	if err := config.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
 		log.Printf("%+v", err)
-		os.Exit(1)
 	}
 
 	// Create a new instance of the `List` action, which lists out your manifests.
-	client := action.NewList(actionConfig)
+	client := action.NewList(config)
 
 	// List only Helm Charts that are deployed
 	client.Deployed = true
 	results, err := client.Run()
 	if err != nil {
 		log.Printf("%+v", err)
-		os.Exit(1)
 	}
 
-	for _, rel := range results {
-		log.Printf("%+v", rel)
+	for _, result := range results {
+		log.Printf("%+v", result)
 	}
 }
